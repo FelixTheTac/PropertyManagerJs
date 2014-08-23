55c4023f7b044c7c947a32eeb8e625e1a6b2df39
Edgar Sanchez <edgar0103sanchez@yahoo.com>
2014-08-23 08:49:27 -0500

    AOP libraries added for unit testing web services.  Ninject and Moq added for IOC and Mocking
    AOP libraries will be used in order to insert functionality as required
    into unit testing scenarios.  Ninject will be used as the testing IOC
    and Moq will be used for mocking web services.

---
+++ Web Service/PropertyManager/PropertyManager.Web.Tests/Controllers/GenericControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropertyManager.Web.Tests.Controllers
+{
+    [TestClass]
+    public class GenericControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
